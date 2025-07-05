f105e8d810d3003449204c047a63971fcd73b5cf
BBoyeon <2boyeon2@gmail.com>
2025-07-05 21:26:49 +0900

    로그인페이지중간저장

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,46 @@ import { useNavigate } from "react-router-dom";
 import '../styles/Login.css'
 
 function Login(){
-  return <div>로그인 페이지</div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setMessage("이메일과 비밀번호를 모두 입력해주세요.");
+      return;}
+
+    };
+
+
+  return (
+    <div className="login-background">
+     <form onSubmit={handleLogin} className="login-container">
+       <h2 className="login-title">🔐로그인</h2>
+       <input
+         type="email"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+         placeholder="이메일 입력"
+         className="login-input"
+         required
+       />
+       <input
+         type="password"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         placeholder="비밀번호 입력"
+         className="login-input"
+         required
+       />
+       <button type="submit" className="login-button">로그인</button>
+      {message && <p className="login-message">{message}</p>}
+    </form>
+    </div>
+  );
 }
 
 export default Login;
