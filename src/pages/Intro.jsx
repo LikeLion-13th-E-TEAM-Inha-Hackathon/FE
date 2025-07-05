420b341877790080ad0639fa92d454637592d5ad
BBoyeon <2boyeon2@gmail.com>
2025-07-05 18:05:25 +0900

    intro 페이지 완성

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -7,16 +7,20 @@ import '../styles/Intro.css'
 function Intro(){
     const navigate = useNavigate()
 
-
     return(
       <div className = "intro-background">
         <div className = "intro-header">
-            <img src ={logo} alt="logo" className="intro-img"/>
-            <p className="intro-explanation">우리가 가까워지는 시간</p>
+            <img src ={logo} alt="logo" className="intro-logo"/>
+            <p className="intro-explanation">우리가 가까워지는 시간. <br></br>
+              지금 패밀로그와 함께 행복한 시간을 만드세요!
+            </p>
         </div>
         <div className="intro-buttons">
             <button onClick={()=>navigate('/login')} className="intro-login">로그인</button>
             <button onClick={()=>navigate('/SignUp')} className="intro-signup">회원가입</button>
+        </div>
+        <div className="intro-bottom">
+          <p>© 2025 Team FamilEE</p>
         </div>
       </div>
 
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+function SignUp() {
+  return <div>회원가입 페이지</div>
+}
+
+export default SignUp
